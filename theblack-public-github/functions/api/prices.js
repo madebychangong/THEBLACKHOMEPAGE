275f097aa7df4ae58940b706830d57f5f10c1511
madebychangong <ppylgr@gmail.com>
2026-05-14 00:35:45 +0900

    Delete theblack-public-github directory

--- theblack-public-github/functions/api/prices.js
+++
@@ -1,7 +0,0 @@
-import { json } from "../_shared/response.js";
-import { readPrices, toPublicPrices } from "../_shared/prices-store.js";
-
-export async function onRequestGet({ env }) {
-  const prices = await readPrices(env);
-  return json(toPublicPrices(prices));
-}
